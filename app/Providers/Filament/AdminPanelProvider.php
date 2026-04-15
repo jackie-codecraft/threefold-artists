7708b0cc10e235038be9b4292f3a537b5e7a3621
Jackie <jackie@sc-codecraft.com>
2026-04-15 18:04:51 +0000

    Separate Filament admin sidebar theme

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -44,7 +44,7 @@ class AdminPanelProvider extends PanelProvider
                 'danger'  => Color::Rose,
             ])
             ->font('Source Sans 3', provider: \Filament\FontProviders\GoogleFontProvider::class)
-            ->theme(asset('css/filament/admin/theme.css'))
+            ->viteTheme('resources/css/filament/admin/theme.css')
             ->navigationGroups([
                 'Content',
                 'People',
