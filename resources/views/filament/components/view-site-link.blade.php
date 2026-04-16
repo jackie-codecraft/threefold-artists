d780170ca31afbf52c021f0c07eb292c5517d72d
Jackie <jackie@sc-codecraft.com>
2026-04-16 06:21:52 +0000

    Add admin view site link

---
+++ resources/views/filament/components/view-site-link.blade.php
@@ -0,0 +1,9 @@
+<ul class="flex items-center">
+    <x-filament-panels::topbar.item
+        :icon="'heroicon-o-arrow-top-right-on-square'"
+        :should-open-url-in-new-tab="true"
+        :url="route('home')"
+    >
+        View Site
+    </x-filament-panels::topbar.item>
+</ul>
