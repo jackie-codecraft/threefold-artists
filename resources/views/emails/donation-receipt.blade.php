2339a060f7dccfb23203a927f234b95f08f75aeb
jackie-codecraft <samcharmy@gmail.com>
2026-08-25 15:09:56 +0000

    feat: thank donors in receipt email

--- resources/views/emails/donation-receipt.blade.php
+++ resources/views/emails/donation-receipt.blade.php
@@ -8,7 +8,7 @@
         </p>
 
         <p style="color: #666; line-height: 1.6;">
-            Thank you for your generous donation to {{ $organization['legal_name'] }}. Your support helps us bring live performing arts to communities that need them most.
+            Thank you for your generous donation to {{ $organization['legal_name'] }}. Your support helps us bring live performing arts to communities that need them most. Thank you for helping keep theatre alive.
         </p>
 
         <div style="margin: 30px 0; padding: 20px; background: #F5F0E8; border-radius: 12px;">
